30e39a8104061a6d773b3d3a15c1e55055306430
Tingyang Chen <56511731+TiyCHEN@users.noreply.github.com>
2024-02-07 22:38:29 +0800

    Update Header.vue

--- GVEX-demo-frontend/src/components/Header.vue
+++ GVEX-demo-frontend/src/components/Header.vue
@@ -4,9 +4,6 @@
         <span style="font-size: 32px; font-weight: bold;"> GVEX </span>
     </el-col>
     <el-col :span="20"><div class="grid-content ep-bg-purple-light" /></el-col>
-    <el-col :span="2"><div class="grid-content ep-bg-purple" />
-      <el-icon :size="36" style="vertical-align: middle; padding-left: 70%;"><User /></el-icon>
-    </el-col>
   </el-row>
 </template>
 
