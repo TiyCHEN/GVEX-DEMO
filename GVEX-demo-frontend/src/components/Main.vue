ee088936c47afc56ca7eec73e6c50161225acfbd
Tingyang Chen <56511731+TiyCHEN@users.noreply.github.com>
2024-02-07 22:38:42 +0800

    Update Main.vue

--- GVEX-demo-frontend/src/components/Main.vue
+++ GVEX-demo-frontend/src/components/Main.vue
@@ -1,11 +1,11 @@
 <template>
-  <el-row>
+  <el-row >
     <el-col :span="18">
       <Origin :originData="receivedVariable" :explainData="receivedExplainVariable"/>
     </el-col>
     <el-col :span="6">
       <el-container class="container">
-          <el-header style="border: none; height: 42px; background-color: #FFFFFF; border-bottom: #000000 2px solid;">
+          <el-header style="border: none; height: 5%; background-color: #FFFFFF; border-bottom: #000000 2px solid;">
             <el-col :span="24">
               <div class="m-header">
               <span class="span-header">Explanation Details</span>
@@ -13,52 +13,69 @@
             </el-col>
           </el-header>
           <el-main style="background-color: #FAFAFA;">
-            <el-row style="height: 450px;">
-                <el-table :data="tableData_1" stripe style="width: 100%" :highlight-current-row="true" :row-style="{height:'60px'}" :cell-style="{fontSize:'24px', textAlign: 'center'}"
-              :header-cell-style="{fontSize:'24px' , color: 'black', textAlign: 'center'}">
-                <el-table-column prop="status" label="Status" width="100">
+            <el-row style="height: 41%;">
+              <el-col :span="24" style="width: 100%;">
+                <el-table :data="tableData_1" stripe style="width: 95%" :highlight-current-row="true" :row-style="{height:'20px'}" :cell-style="{fontSize:'1vw', textAlign: 'center'}"
+              :header-cell-style="{fontSize:'1.2vw' , color: 'black', textAlign: 'center'}">
+                <!-- <el-table-column prop="status" label="Status" width="100">
                   <template #default="scope">
                     <span>
                       <i class="dotClass" style="background-color: #67C23A"></i>
                     </span>
                   </template>
-                </el-table-column>>
-                <el-table-column prop="id" label="ID" width="80" />
-                <el-table-column prop="label" label="Label" width="165" />
-                <el-table-column prop="size" label="Size" width="105"/>
-                <el-table-column label="Detail">
+                </el-table-column>> -->
+                <el-table-column prop="id" label="ID" width="45" />
+                <el-table-column prop="label" label="Label" width="90" />
+                <el-table-column prop="size" label="Size" width="60"/>
+                <el-table-column label="Detail" width="70">
                     <template #default="scope">
-                      <el-button type="primary" plain>Detail</el-button>
+                      <el-button type="primary" plain style="width: 100%;">Detail</el-button>
                     </template>
               </el-table-column>
               </el-table>
+              </el-col>
             </el-row>
-            <el-row style="height: 40px;">
-              <div class="d-header">
-                <span class="span-header">Baseline Explanations</span> 
-              </div>
-            </el-row>
-            <el-row style="height: 40px; margin-top: 5px; margin-bottom: 5px;">
-                    <el-button type="primary" @click="showAlgorithm('subgraphx')" style="font-family: 'Times New Roman', Times, serif; color:black; width: 160px; margin: auto; font-weight: bold;" plain>
-                    <span style="font-size: 24px; font-weight: bold; vertical-align: middle;"> SubgraphX </span></el-button>
-                    <el-button type="primary" @click="showAlgorithm('gnnexp')" style="font-family: 'Times New Roman', Times, serif; color:black; width: 160px; margin: auto; font-weight: bold;" plain>
-                    <span style="font-size: 24px; font-weight: bold; vertical-align: middle;"> GNNExplainer </span></el-button>
-                    <el-button type="primary" @click="showAlgorithm('gstarx')" style="font-family: 'Times New Roman', Times, serif; color:black; width: 160px; margin: auto; font-weight: bold;" plain>
-                    <span style="font-size: 24px; font-weight: bold; vertical-align: middle;"> GstarX </span></el-button>
-                  </el-row>
-            <el-row style="height: 550px; width:100%">
-              <el-col :span="24">
-                <div class="baseline-page" id="baseline">
-                  <el-row style="height: 330px;">
-                    <div class="origin-graph" style="width: 100%;" id="origin">
-                    </div>  
-                  </el-row>
-                  <el-divider style="border-top: 1px solid black;"/>
-                  <el-row style="height: 210px;">
-                    <div class="explain-graph" style="width: 100%;" id="explain">
-                    </div>  
-                  </el-row>
+            <el-row style="height: 5vh; width: 100%;">
+              <el-col :span="24" style="width: 100%;">
+                <div class="d-header">
+                  <span class="span-header">Baseline Explanations</span> 
                 </div>
+              </el-col>
+            </el-row>
+            <el-row style="height: 5%; margin-top: 5px; margin-bottom: 5px;">
+              <el-col :span="8" style="width: 100%">
+                <el-button type="primary" @click="showAlgorithm('subgraphx')" style="font-family: 'Times New Roman', Times, serif; color:black; width: 95%; margin: auto; font-weight: bold;" plain>
+                <span style="font-size: 0.8vw; font-weight: bold; vertical-align: middle;"> SubgraphX </span></el-button>
+              </el-col>
+              <el-col :span="8" style="width: 100%">
+                <el-button type="primary" @click="showAlgorithm('gnnexp')" style="font-family: 'Times New Roman', Times, serif; color:black; width: 95%; margin: auto; font-weight: bold;" plain>
+                <span style="font-size: 0.8vw; font-weight: bold; vertical-align: middle;"> GNNExplainer </span></el-button>
+              </el-col>
+              <el-col :span="8" style="width: 100%">
+                <el-button type="primary" @click="showAlgorithm('gstarx')" style="font-family: 'Times New Roman', Times, serif; color:black; width: 95%; margin: auto; font-weight: bold;" plain>
+                <span style="font-size: 0.8vw; font-weight: bold; vertical-align: middle;"> GstarX </span></el-button>
+              </el-col>
+            </el-row>
+            <el-row style="height: 45%; width:100%">
+              <el-col :span="24">
+                  <el-row style="height: 40%;">
+                    <el-col :span="24" style="width: 100%; height: 100%;">
+                      <div class="origin-graph" style="width: 100%;" id="origin">
+                      </div>  
+                    </el-col>
+
+                  </el-row>
+                  <el-row style="height: 5%;">
+                    <el-col  :span="24" style="width: 100%; height: 100%;">
+                      <el-divider style="border-top: 1px solid black;"/>
+                    </el-col>
+                  </el-row>
+                  <el-row style="height: 40%;">
+                    <el-col :span="24" style="width: 100%; height: 100%;">
+                      <div class="explain-graph" style="width: 100%;" id="explain">
+                      </div> 
+                    </el-col>
+                  </el-row>
               </el-col>
 
             </el-row>
@@ -138,8 +155,8 @@
       
         const chartContainer = document.createElement('div');
         chartContainer.id = 'explainGraph_baselines';
-        chartContainer.style.width = '95%';
-        chartContainer.style.height = '200px'; // 设置图表容器高度
+        chartContainer.style.width = '90%';
+        chartContainer.style.height = '15vh'; // 设置图表容器高度
         chartContainer.style.display = 'inline-block';
         document.getElementById('origin').appendChild(chartContainer);
 
@@ -162,7 +179,7 @@
               },
               roam: true,
               force: {
-                repulsion: 100
+                repulsion: 10
               },
               animation: true,
               itemStyle: {
@@ -207,7 +224,7 @@
         const chartContainer = document.createElement('div');
         chartContainer.id = 'originGraph';
         chartContainer.style.width = '98%';
-        chartContainer.style.height = '300px'; // 设置图表容器高度
+        chartContainer.style.height = '15vh'; // 设置图表容器高度
         chartContainer.style.display = 'inline-block';
         while (document.getElementById('origin').firstChild) {
           document.getElementById('origin').removeChild(document.getElementById('origin').firstChild);
@@ -233,7 +250,7 @@
               },
               roam: true,
               force: {
-                repulsion: 50
+                repulsion: 5
               },
               animation: true,
               itemStyle: {
@@ -259,6 +276,7 @@
     })
     .catch(error => {
       // 处理请求错误
+      alert('Please support the algorithm first')
       console.error(error);
     });
   }
@@ -270,31 +288,37 @@
 
 <style scoped>
   .container{
-    height: 94.2vh;
     border-right: #606266 4px solid;
   }
   .m-header{
-    height: 40px;
+    height: 4.6vh;
+    width:100%;
     text-align: center;
-    line-height: 40px;
+    line-height: 4.6vh;
     background-color: #F2F3F5;
     /* border-right: #000000 1px solid; */
   }
   .d-header{
-    height: 40px;
     width:100%;
+    height: 100%;
     text-align: center;
-    line-height: 40px;
+    line-height: 100%;
     background-color: #F2F3F5;
     /* border-right: #000000 2px solid; */
     border-bottom: #000000 2px solid;
-    border-top: #000000 2px solid;
   }
 
   .span-header{
+    display: flex;
+    justify-content: center; /* 水平居中 */
+    align-items: center; /* 垂直居中 */
+    width: 100%;
+    height: 100%;
     font-family: 'Times New Roman', Times, serif; 
     font-weight: bold;  
-    font-size: 30px;
+    font-size: 1.7vw;
+    line-height: 100%;
+    margin: auto
   }
   .dotClass {
     width:12px;
